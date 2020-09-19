e80a67989cd4e0eaf4dd46ab8c6b7b3841a47dac
AlexeiZenin <zalexei7@gmail.com>
2020-09-19 13:21:41 -0400

    Init repo

---
+++ src/test/java/com/zenin/genericproto/GenericProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenin.genericproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
